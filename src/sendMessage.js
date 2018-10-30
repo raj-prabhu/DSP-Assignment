705714b024b9f3eceeb5be8b25b4f16a171a621d
Raj Prabhu <raj.mprabhu@gmail.com>
2018-10-30 10:13:12 +0530

    Delete sendMessage.js

--- src/sendMessage.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-module.exports = (senderPSID, message) => {
-  return axios.post('https://graph.facebook.com/v2.6/me/messages', {
-    access_token: process.env.PAGE_ACCESS_TOKEN,
-    recipient: { id: senderPSID },
-    message: message
-  });
-};
